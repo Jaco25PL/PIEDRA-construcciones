e014af75e8e0f8ff0b9aab3acfd6fdab6f1dd798
PiedraL <joaquinlangone12@gmail.com>
2024-03-14 03:18:43 -0300

    header done (basic)

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import { Header } from './components/Header'
 import { Navigation } from './components/Navigation'
 
 function App() {
@@ -6,30 +7,9 @@ function App() {
   return (
     <div className='w-full'>
       <Navigation/>
-      <main>
+      <main className='w-full'>
 
-      <section>
-        <div>
-          <h1>Entregamos la Maxima Calidad en todo lo que hacemos</h1>
-        </div>
-
-        <div>
-          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tempora a ipsam optio tenetur sapiente neque porro necessitatibus, dicta et repellat nostrum ex dolores. Doloremque nobis ipsa iste deserunt in a, vitae odit, beatae pariatur, dolorem recusandae quam voluptates at sunt reiciendis quidem? Quod possimus repudiandae dolore tempore quas, suscipit blanditiis! </p>
-        </div>
-
-        <div>
-          <img src="" alt="header-image"/>
-        </div>
-
-        <div>
-          <button></button>
-        </div>
-
-        <div>
-          <h2></h2>
-          <span></span>
-        </div>
-      </section>
+        <Header/>
 
       </main>
     </div>
--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,27 +1,37 @@
 
 export function Navigation() {
 
+  const scrollToTop = () => {
+    window.scrollTo({ top: 0, behavior: 'smooth' })
+  }
+
+  // const scrollTo = () => {
+  //   const element = "element"
+  //   element?.window.scrollTo({top:element.offsetTop, behavior: "smooth"})
+
+  // }
+
     return (
-        <header className='w-full'>
+        <header className='z-50 w-full backdrop-blur-md bg-slate-50 bg-opacity-50 sticky top-0'>
             <div className='w-full'>
               <nav className='mx-32 py-5  flex flex-row justify-between items-center'>
                 <div>
-                  <span className='font-bold text-3xl cursor-pointer'>
+                  <button onClick={scrollToTop} className='font-bold text-3xl cursor-pointer'>
                     PIEDRA
-                  </span>
+                  </button>
                 </div>
                 <div>
                   <ul className='nav-link flex flex-row gap-8 font-semibold [&>li]:relative [&>li:after]:absolute [&>li:after]:content-[""] [&>li:after]:top-8 [&>li:after]:left-0 [&>li:after]:w-full [&>li:after]:h-1 [&>li:after]:bg-gray-900 [&>li:after]:opacity-0 [&>li:after]:transition-opacity [&>li:after]:duration-300 hover:[&>li:after]:opacity-100'>
-                    <li><a href="#home">Home</a></li>
-                    <li><a href="#services">Servicios</a></li>
-                    <li><a href="#about">Nosotros</a></li>
-                    <li><a href="#projects">Proyectos</a></li>
+                    <li><button onClick={scrollToTop}>Home</button></li>
+                    <li><a href="services">Servicios</a></li>
+                    <li><a href="about">Nosotros</a></li>
+                    <li><a href="projects">Proyectos</a></li>
                   </ul>
                 </div>
-                <div className='bg-gray-100 hover:bg-gray-300 text-gray-800 font-bold py-2 px-4 rounded-lg inline-flex items-center cursor-pointer transition-all duration-200'>
-                  <span className='font-bold text-xl'>
+                <div className='bg-gray-100 hover:bg-red-500 hover:text-gray-100 text-gray-800 font-bold py-2 px-4 rounded-lg inline-flex items-center cursor-pointer transition-all duration-200'>
+                  <a href="contacts" className='font-bold text-xl'>
                     Contactanos
-                  </span>
+                  </a>
                 </div>
               </nav>
             </div>
