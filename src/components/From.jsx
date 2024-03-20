e927ff28546a6da6fbef3e61b507be863d019d4d
PiedraL <joaquinlangone12@gmail.com>
2024-03-19 23:14:39 -0300

    Form view & validations

--- src/components/From.jsx
+++ src/components/From.jsx
@@ -1,38 +1,84 @@
+import { useForm } from "react-hook-form"
 
 export function Form() {
 
-    const handleSubmit = (e) => {
-        e.preventDefault()
+    const { 
+        register, 
+        handleSubmit,
+        formState: { errors },
+    } = useForm()
+
+    const onSubmit = (data) => {
+        console.log(data)
     }
+
 
     return(
         <div className="w-full pb-10 ">
             
             <h2 className="w-fit mx-auto mb-6">NOS QUIERES DECIR ALGO?</h2>
 
-            <div className="bg-gradient-to-t to-gray-400 to-transparent p-10 sm:max-w-xl mx-auto sm:rounded-xl [&>form]:flex [&>form]:flex-col [&>form]:gap-4 [&>form]:w-full ">
-                <form id="myForm" onSubmit={() => handleSubmit()} className="[&>*]:p-3 [&>*]:rounded-lg focus:[&>*]:outline focus:[&>*]:outline-gray-300 focus:[&>*]:outline-4  [&>textarea]:h-52 [&>textarea]:resize-none">
+            <div className="bg-gradient-to-t to-gray-400 from-transparent p-10 sm:max-w-xl mx-auto sm:rounded-xl [&>form]:flex [&>form]:flex-col [&>form]:gap-4 [&>form]:w-full ">
+                <form onSubmit={handleSubmit(onSubmit)} id="myForm" className=" [&_input]:w-full [&_textarea]:w-full  [&_input]:p-3 [&_input]:rounded-lg focus:[&_input]:outline focus:[&_input]:outline-gray-300 focus:[&_input]:outline-4 [&_textarea]:p-3 [&_textarea]:rounded-lg focus:[&_textarea]:outline focus:[&_textarea]:outline-gray-300 focus:[&_textarea]:outline-4 [&_textarea]:h-52 [&_textarea]:resize-none">
 
-                    <input type="text" id="name" name="name" placeholder="NOMBRE" required />
+                    <div>
+                        <input {...register("name", {
+                            required: "Se requiere un Nombre",
+                            minLength: {
+                                value: 3,
+                                message: "Introduce un nombre real"
+                            }
+                        })} type="text" placeholder="NOMBRE"  />
+                        {  errors.name && (
+                            <div className="text-red-600 font-medium">{errors.name.message}</div>
+                        ) }
+                    </div>
+                    
+                    <div>
+                        <input {...register("phone", {
+                            validate: (value) => {
+                                if(value && value.length !== 9){
+                                    return "Porfavor introduzca un numero valido"
+                                }
+                            }
+                        })}
+                        type="tel" placeholder="TELEFONO"  />
+                        { errors.phone && (
+                            <div className="text-red-600 font-medium">{errors.phone.message}</div>
+                        )}
+                
+                    </div>
+                    
+                    <div>
+                        <input {...register("email", {
+                            required: "El email es requerido",
+                            pattern: {
+                                value: /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/,
+                                message: "Correo invalido"
+                            },
+                        })} type="text" placeholder="EMAIL" />
+                        {  errors.email && (
+                            <div className="text-red-600 font-medium">{errors.email.message}</div>
+                        ) }
+                    </div>
 
-                    <input type="tel" id="phone" name="phone" placeholder="TELEFONO" required />
-
-                    <input type="email" id="email" name="email"  placeholder="EMAIL"required />
-
-                    <textarea id="message" name="message" placeholder="ESCRIBA UN MENSAJE" required></textarea>
-
-                    <button type="submit" className="bg-red-500 w-fit text-gray-50 font-semibold mx-auto mt-6 hover:transform hover:scale-110 active:scale-90 hover:transition-all duration-300 ease-in-out ">ENVIAR</button>
+                    <div>
+                        <textarea {...register("message", {
+                            required: "Por favor, no deje la hoja en blanco",
+                            minLength: {
+                                value: 20,
+                                message: "Porfavor, detalle sus dudas"
+                            }
+                        })} placeholder="ESCRIBA UN MENSAJE" />
+                        { errors.message && (
+                            <div className="text-red-600 font-medium">{errors.message.message}</div>
+                        )}
+                    </div>
+                    
+                    <button type="submit" className="bg-red-500 px-4 py-2 rounded-xl w-fit text-gray-50 font-semibold mx-auto mt-6 hover:transform hover:scale-110 active:scale-90 hover:transition-all duration-300 ease-in-out ">ENVIAR</button>
                 </form>
-            </div>
+            </div> 
         
         </div>
     )
 }
-
-    {/* <form action="">
-                <input type="text" placeholder="NOMBRE" />
-                <input type="email" placeholder="EMAIL" />
-                <input type="phone" placeholder="TELEFONO" />
-                <textarea name="message" cols="30" rows="10" placeholder="CUAL ES LA RAZON DE ESTE MENSAJE?"></textarea>
-                <button type="submite">ENVIAR</button>
-            </form> */}
