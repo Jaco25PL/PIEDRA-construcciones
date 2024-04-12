9ae3aab8966ca9383b8e1384326bbd6637734ee4
PiedraL <joaquinlangone12@gmail.com>
2024-04-12 13:47:55 -0300

    Form validation fixed

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,19 +1,23 @@
 /* eslint-disable react/prop-types */
+import { useRef } from "react"
 import { useForm } from "react-hook-form"
 import { motion } from "framer-motion"
 
 export function Form({ fromRef }) {
 
+    const sendRef = useRef(null)
+
     const { 
         register, 
-        // handleSubmit,
+        handleSubmit,
         formState: { errors },
     } = useForm()
 
-    // const onSubmit = (data) => {
-    //     console.log(data)
-    // }
-
+    const onsubmit = () => {
+        if(sendRef.current){
+            console.log(sendRef.current.submit())
+        }   
+    }
 
     return(
 
@@ -29,8 +33,7 @@ export function Form({ fromRef }) {
             <h2 className="text-center w-fit mx-auto mb-6">CONTACTATE CON NOSOTROS</h2>
 
             <div className=" border-solid border-2 border-gray-200 px-5 sm:px-10 py-10 sm:max-w-xl mx-auto sm:rounded [&>form]:flex [&>form]:flex-col [&>form]:gap-4 [&>form]:w-full ">
-                {/* <form action="https://getform.io/f/lakmkona" method="POST" onSubmit={handleSubmit(onSubmit)} id="myForm" className=" [&_input]:w-full [&_textarea]:w-full  [&_input]:p-3 [&_input]:rounded-lg focus:[&_input]:outline focus:[&_input]:outline-gray-300 focus:[&_input]:outline-4 [&_textarea]:p-3 [&_textarea]:rounded-lg focus:[&_textarea]:outline focus:[&_textarea]:outline-gray-300 focus:[&_textarea]:outline-4 [&_textarea]:h-52 [&_textarea]:resize-none"> */}
-                <form action="https://getform.io/f/lakmkona" method="POST" id="myForm" className=" [&_input]:w-full [&_textarea]:w-full focus:[&_input]:bg-gray-50 [&_input]:bg-gray-100 focus:[&_textarea]:bg-gray-50 [&_textarea]:bg-gray-100 focus:[&_input]:border-gray-50 focus:[&_textarea]:border-gray-50 [&_input]:border-l-2 [&_input]:border-solid [&_input]:border-gray-500 [&_textarea]:border-l-2 [&_textarea]:border-solid [&_textarea]:border-gray-500 [&_input]:p-3 [&_input]:rounded focus:[&_input]:outline focus:[&_input]:outline-gray-300 focus:[&_input]:outline-2 [&_textarea]:p-3 [&_textarea]:rounded focus:[&_textarea]:outline focus:[&_textarea]:outline-gray-300 focus:[&_textarea]:outline-2 [&_textarea]:h-52 [&_textarea]:resize-none">
+                <form ref={sendRef}  action="https://getform.io/f/lakmkona" method="POST"  onSubmit={handleSubmit(onsubmit)} id="myForm" className=" [&_input]:w-full [&_textarea]:w-full focus:[&_input]:bg-gray-50 [&_input]:bg-gray-100 focus:[&_textarea]:bg-gray-50 [&_textarea]:bg-gray-100 focus:[&_input]:border-gray-50 focus:[&_textarea]:border-gray-50 [&_input]:border-l-2 [&_input]:border-solid [&_input]:border-gray-500 [&_textarea]:border-l-2 [&_textarea]:border-solid [&_textarea]:border-gray-500 [&_input]:p-3 [&_input]:rounded focus:[&_input]:outline focus:[&_input]:outline-gray-300 focus:[&_input]:outline-2 [&_textarea]:p-3 [&_textarea]:rounded focus:[&_textarea]:outline focus:[&_textarea]:outline-gray-300 focus:[&_textarea]:outline-2 [&_textarea]:h-52 [&_textarea]:resize-none">
 
                     <div>
                         <input {...register("name", {
